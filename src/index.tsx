4959c518e1a03d40ebbb95d067f36479b0b9c321
Ivan Zuev <ivan-zuev-97@mail.ru>
2024-01-30 16:47:33 +0300

    add initial structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
+import {ConfigProvider} from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-import App from '@/App';
+import App from './app/App';
+
+import './app/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <ConfigProvider
+            theme={{
+                token: {
+                    fontSize: 18,
+                    fontFamily: '\'Montserrat\', sans-serif'
+                }
+            }}
+        >
+            <App />
+        </ConfigProvider>
     </React.StrictMode>,
 );
